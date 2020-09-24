5beff272bb93bf4b5a7e3127edadca95fa461316
Awsthyn <aguswagner008@gmail.com>
2020-09-24 08:08:44 -0300

    user can choose size, through query

--- Board.js
+++ Board.js
@@ -6,13 +6,12 @@ function Board(){
 
 }
 
-Board.prototype.generateBoard = function(){
+Board.prototype.generateBoard = function(size){
     this.value = []
-
     function generateColumn(){
         let column = []
-        for(j=0; j < 27; j++){
-            if(j === 0 || j === 26) column.push('|')
+        for(j=0; j < size/*27*/; j++){
+            if(j === 0 || j === size-1/*26*/) column.push('|')
             else{
                 let cell = new Cell()
                 column.push(cell.value)}
@@ -22,16 +21,16 @@ Board.prototype.generateBoard = function(){
 
     function generateHorizontallBorders(){
         let border = []
-        for(k=0; k < 27; k++){
-            if(k == 0 || k == 26) border.push('+')
+        for(k=0; k < size/*27*/; k++){
+            if(k == 0 || k == size-1/*26*/) border.push('+')
             else border.push('-')
         }
         return border
     }
 
 
-    for(i = 0; i <27; i++){
-        if(i == 0 || i == 26) this.value.push(generateHorizontallBorders())
+    for(i = 0; i <size/*27*/; i++){
+        if(i == 0 || i == size-1/*26*/) this.value.push(generateHorizontallBorders())
         else this.value.push(generateColumn())
     }
     return this.value
--- index.js
+++ index.js
@@ -12,9 +12,13 @@ if (process.env.NODE_ENV === "production") {
   }*/
 
 app.get('/', (req, res)=>{
+    const {size} = req.query
+
     let board = new Board();
+    let problem;
+    Number(size) > 5 && Number(size) < 35 ? problem = board.generateBoard(Number(size) + 2) : problem = board.generateBoard(27)
     var html = fs.readFileSync(__dirname + "/index.html", "utf8");
-    html = html.replace(`{problem}`, JSON.stringify({problem: board.generateBoard()}))
+    html = html.replace(`{problem}`, JSON.stringify({problem}))
     html = html.replace(`{solution}`, JSON.stringify({solution: board.resolve()}))
     res.status(200)
     
